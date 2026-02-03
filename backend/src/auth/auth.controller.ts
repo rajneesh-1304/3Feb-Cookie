b1e76c7391d71d661aa9065f4fa163ead36c2dfc
Rajneesh <kumarrajneesh1304@gmail.com>
2026-02-03 18:07:41 +0530

    First

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Post, Req, UseGuards } from '@nestjs/common';  
+
+import { LocalAuthGuard } from './local.guard';
+import { AuthService } from './auth.service';
+import { Public } from 'src/decorators/public.decorator';
+
+@Controller('auth')  
+export class AuthController {  
+    constructor(private authService: AuthService) {}
+//   @UseGuards(LocalAuthGuard)  
+  @Post('login')  
+  @Public()
+  @UseGuards(LocalAuthGuard)
+  login(@Req() req: Request): any {  
+    return this.authService.signIn(req); 
+  }  
+
+//   @Post('logout')  
+//   logout(@Request() req): any {  
+//     req.session.destroy();  
+//     return { message: 'Logout successful' };  
+//   }  
+}
